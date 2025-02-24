b58773dfc18adc80d4a401572624eab9dc5e1fae
Joao <joaovitorsena116@gmail.com>
2025-02-23 22:57:18 -0300

    Adição do NavBar e criação do botão

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,14 @@ import sol_desvelado from './assets/sol_desvelado.jpg'
 import anel from './assets/anel.jpg'
 import fim_da_infancia from './assets/end.jpg'
 
-
+import NavBar from './components/Layout/NavBar'
 
 function App() {
 
   return (
     <>
-      <div className="card">
-       <h1>PW3 - WEBAPP - LIVRARIA</h1>
-       <BookCard
-        titulo='As Cavernas de Aço' 
-        autor='Isaac Azimov'
-        imagem={cavernas_aco}/>
-        <BookCard
-        titulo='Senhor Dos Anéis' 
-        autor='J. R. R. Tolkien'
-        imagem={anel}/>
-         <BookCard
-        titulo='O Fim da Infancia' 
-        autor='Arthur C. Clark'
-        imagem={fim_da_infancia}/>
-         <BookCard
-        titulo='O Sol Desvelado' 
-        autor='Isaac Azimov'
-        imagem={sol_desvelado}/>
+      < div >
+      <NavBar />
       </div>
     </>
   )
